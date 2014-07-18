246b94955897717dd22d74b0477a6be392835957
Matt Jenkins <matt@majenko.co.uk>
2014-07-18 15:09:55 +0100

    Fixed dtostrf

--- hardware/pic32/cores/pic32/WString.cpp
+++ hardware/pic32/cores/pic32/WString.cpp
@@ -29,9 +29,15 @@
 
 char *dtostrf(double __val, signed char __width, unsigned char __prec, char *__s) {
     int i = (int)__val;
-    double trunc = __val - i;
-    trunc *= __prec;
-    snprintf(__s, __width, "%d.%d", i, (int)trunc);
+    double trunc = __val - (float)i;
+    if (trunc < 0) {
+        trunc = 0 - trunc;
+    }
+    for (int q = 0; q < __prec; q++) {
+        trunc *= 10.0;
+    }
+    unsigned int dec = (int)trunc;
+    snprintf(__s, __width, "%d.%d", i, dec);
     return __s;
 }
 
