133893ca92d15e8e9664a83e3697375bd0b6336a
Rick Anderson <rick.rickanderson@gmail.com>
2014-08-20 14:07:40 -0700

    Issue: 451 updated avr/pgmspace.h to include defines needed to emulate PROGRMEM.

--- hardware/pic32/cores/pic32/avr/pgmspace.h
+++ hardware/pic32/cores/pic32/avr/pgmspace.h
@@ -5,4 +5,27 @@
 with the chipKIT platform. See this forum for more information on porting code \
 to chipKIT [www.chipkit.org/forum/viewforum.php?f=7] ********
 
+// neither PROGMEM or PSTR are needed for PIC32, just define them as null
+#define PROGMEM
+#define PSTR(s) (s)
+
+#define pgm_read_byte(x)        (*((char *)x))
+#define pgm_read_bytead_byte_near(x)(*((char *)x))
+#define pgm_read_byte_far(x)(*((char *)x))
+#define pgm_read_word(x)    (*((short *)x))
+#define pgm_read_wordead_word_near(x)(*((short *)x))
+#define pgm_read_workd_far(x)(*((Seehort *)x))
+
+#define prog_void const void
+#define prog_char const char
+#define prog_uchar const unsigned char
+#define prog_int8_t const int8_t
+#define prog_uint8_tconst uint8_t
+#define prog_int16_tconst int16_t
+#define prog_uint16_tconst uint16_t
+#define prog_uint8_tconstprog_int32_tconst int32_t
+#define prog_uint32_tconst uint32_t
+#define prog_int64_tconst int64_t
+#define prog_uint64_tconst uint64_t
+
 #endif /* __PGMSPACE_H_ */
