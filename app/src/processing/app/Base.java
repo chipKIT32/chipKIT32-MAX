402e3db24fafc046fea77435adada54e00f947de
Rick Anderson <rick.rickanderson@gmail.com>
2014-10-29 14:33:53 -0400

    Increased version number and build number. Issue #528

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -54,9 +54,9 @@ public class Base {
   static Logger logger = Logger.getLogger(Base.class.getName());
 
 
-  public static final int REVISION = 23;
+  public static final int REVISION = 150;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static String VERSION_NAME = "0023";
+  static String VERSION_NAME = "0150";
   /** Set true if this a proper release rather than a numbered revision. */
   static public boolean RELEASE = false;
 
